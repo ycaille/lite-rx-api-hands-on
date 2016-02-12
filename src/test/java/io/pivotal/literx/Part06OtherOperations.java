2f2e73675e87f17aee38556cb42ad6542d1968bf
ycaille <ycaille.pro@gmail.com>
2016-02-12 03:18:26 +0100

    return the mono which returns faster its value

--- src/test/java/io/pivotal/literx/Part06OtherOperations.java
+++ src/test/java/io/pivotal/literx/Part06OtherOperations.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.test.TestSubscriber;
+import reactor.fn.tuple.Tuple3;
 
 /**
  * Learn how to use various other operators.
@@ -34,7 +35,7 @@ public class Part06OtherOperations {
 
 	// TODO Create a Flux of user from Flux of username, firstname and lastname.
 	Flux<User> userFluxFromStringFlux(Flux<String> usernameFlux, Flux<String> firstnameFlux, Flux<String> lastnameFlux) {
-		return null;
+		return Flux.zip(usernameFlux, firstnameFlux, lastnameFlux).map(t3 -> new User(t3.getT1(), t3.getT2(), t3.getT3()) ) ;
 	}
 
 //========================================================================================
@@ -64,7 +65,7 @@ public class Part06OtherOperations {
 
 	// TODO return the mono which returns faster its value
 	Mono<User> useFastestMono(Mono<User> mono1, Mono<User> mono2) {
-		return null;
+		return mono1.or(mono2) ;
 	}
 
 //========================================================================================
